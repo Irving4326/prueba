801dec401657df396a50f784fd9768e9b78bd204
Irving4326 <43560217+Irving4326@users.noreply.github.com>
2018-10-29 08:50:46 -0700

    Update Eva_9/src/Auto/Automóvil.java

---
+++ Eva_9/src/Auto/Automóvil.java
@@ -0,0 +1,95 @@
+package Auto;
+
+/*
+ * @Irving Gerardo Mancera Mejia
+ */
+public class Automóvil {
+   public  String marca, placa, gasolina, aues, motor;
+   public   double precio, kilometraje;
+   public int cambios,año, llanrepu; 
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getGasolina() {
+        return gasolina;
+    }
+
+    public void setGasolina(String gasolina) {
+        this.gasolina = gasolina;
+    }
+
+    public String getAues() {
+        return aues;
+    }
+
+    public void setAues(String aues) {
+        this.aues = aues;
+    }
+
+    public String getMotor() {
+        return motor;
+    }
+
+    public void setMotor(String motor) {
+        this.motor = motor;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public double getKilometraje() {
+        return kilometraje;
+    }
+
+    public void setKilometraje(double kilometraje) {
+        this.kilometraje = kilometraje;
+    }
+
+    public int getCambios() {
+        return cambios;
+    }
+
+    public void setCambios(int cambios) {
+        this.cambios = cambios;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+    public int getLlanrepu() {
+        return llanrepu;
+    }
+
+    public void setLlanrepu(int llanrepu) {
+        this.llanrepu = llanrepu;
+    }
+    
+    
+    
+    
+    
+}
